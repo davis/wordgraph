10928df199d6e64b3012e9b42bf067a9f2c5b2db
Davis Kim <dchk90@gmail.com>
2014-05-19 15:49:30 -0700

    Move some functionality temporarily into messageFetch

--- public/javascripts/visualizer.js
+++ public/javascripts/visualizer.js
@@ -1,13 +1,36 @@
-var width = 960,
-    height = 500;
+// var width = 960,
+//     height = 500;
 
 // append an svg to the body
 var svg = d3.select('.container').append('svg')
-    .attr('width', width)
-    .attr('height', height)
+    // .attr('width', width)
+    // .attr('height', height)
   .append('g');
 
-var messages = ['hello hi hi davis', 'hi davis', 'hello', 'hello'];
+var testmsgs = [
+  'hello hi hi davis',
+  'hi davis',
+  'hello',
+  'hello',
+  'patrick patrick patrick patrick patrick patrick patrick patrick',
+  'what is up',
+  'nothing much',
+  'more stuff stuff stuff stuff',
+  'stuff unique words words la lalala qwerty'
+];
+
+var messages2 = [
+  'hello hi davis',
+  'hi',
+  'hello',
+  'nothing',
+  'patrick patrick patrick patrick patrick patrick patrick patrick',
+  'patrick patrick stuff',
+  'what is up',
+  'nothing much',
+  'more stuff stuff stuff stuff',
+  'stuff unique words words la lalala qwerty'
+];
 
 
 // countWords: count words in an array of strings and return an object
@@ -15,7 +38,7 @@ var messages = ['hello hi hi davis', 'hi davis', 'hello', 'hello'];
 function countWords(messages) {
   var words = {};
   for(var i = 0; i < messages.length; i++) {
-    var split = messages[i].split(' ');
+    var split = messages[i].message.split(' ');
     for(var j = 0; j < split.length; j++) {
       if(!words[split[j]]) {
         words[split[j]] = 1;
@@ -27,7 +50,7 @@ function countWords(messages) {
 
   var result = {
     columns: ['total'],
-    axis: []
+    axis: ['count']
   };
   for(var word in words) {
     result.columns.push(words[word]);
@@ -36,22 +59,38 @@ function countWords(messages) {
   return result;
 }
 
-console.log(countWords(messages))
+// var chart = c3.generate({
+//   bindto: '.container',
+//   data: {
+//     x: 'count',
+//     columns: [
+//       countWords(testmsgs).axis,
+//       countWords(testmsgs).columns,
+//     ],
+//     groups: [
+//       ['count']
+//     ],
+//     type: 'bar'
+//   },
+//   axis: {
+//     x: {
+//       type: 'categorized',
+//     }
+//   },
+//   transition: {
+//     duration: 1000
+//   }
+// });
 
-var chart = c3.generate({
-  bindto: '.container',
-    data: {
-      columns: [
-        countWords(messages).columns
-      ],
-      type: 'bar'
-    },
-    axis: {
-      x: {
-        type: 'categorized',
-        categories: countWords(messages).axis
-      }
-  }});
+// setTimeout(function () {
+//   chart.load({
+//     columns: [
+//       countWords(messages2).axis,
+//       countWords(messages2).columns,
+//     ],
+//   });
+// }, 1000);
+
 
 
 
