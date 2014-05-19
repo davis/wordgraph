38532078ccacdb7d531e9f52e5b4ab0c66e23ad7
Davis Kim <dchk90@gmail.com>
2014-05-19 16:12:59 -0700

    Add ability to choose conversation via jquery

--- public/javascripts/messageFetch.js
+++ public/javascripts/messageFetch.js
@@ -1,48 +1,53 @@
-var uninteresting = ['the', 'it', 'is', 'we', 'all', 'a', 'an', 'by', 'to', 'you', 'me', 'he', 'she', 'they', 'we', 'how', 'it', 'i', 'are', 'to', 'for', 'of'];
-var messages = [];
-var messageId = 300559876768618; //etc
-var token = 'CAACEdEose0cBAGlgEm88Yox64NgZBgcytY0pMbE0PFrNQ3F5NDEZB0UqD5YbGhStkwdIkUDvPh0LJM4RuM1ZCr3pNFbyrHhDWpqRCS5YJl4hj8ZAGswvn0B7zKAag63eEyn5urtBQZC8hFZBiPxswmc64tTZA5gkg28WzePRgQaw3LFlZAZAYUjM5MCZBJw9nQkQQZD';
+$(function(){
 
-function process(data) {
+  var messages = [];
+  var token = 'CAACEdEose0cBAPsc5EojEPsnGCVpG05fBKSV1N2WrZAJJ8ngZB55cAkWQA82ZBSaGbiOUOcI9rpRxMxT5kAjGmhhSSieTjtXFEuMYYOEtVIrFYC9ZCkuSSdT5P45ZBBvuniyPGZCZCFPDxkZBFhiRr0BRKje0ZBG7ylmeiJ10ImU8k3mpY5DdoPNT5yZCc6qZBF5jUZD';
+
+  function process(data) {
     if(data.data.length) {
-        messages = messages.concat(data.data);
+      messages = messages.concat(data.data);
     }
     if(data.paging && data.paging.next) {
-        console.log('Getting next page');
-        return $.getJSON(data.paging.next).then(process);
+      console.log('Getting next page');
+      return $.getJSON(data.paging.next).then(process);
     }
-}
+  }
 
-$.getJSON(
-     'https://graph.facebook.com/' + messageId + '/comments' +
-     '?format=json&access_token=' + token
-  )
-  .then(process)
-  .then(function() {
-    return messages;
-    // location.href = URL.createObjectURL(blob);
-  })
-  .then(function(messages) {
-    var chart = c3.generate({
-      bindto: '.container',
-      data: {
-        x: 'count',
-        columns: [
-          countWords(messages).axis,
-          countWords(messages).columns,
-        ],
-        groups: [
-          ['count']
-        ],
-        type: 'bar'
-      },
-      axis: {
-        x: {
-          type: 'categorized',
-        }
-      },
-      transition: {
-        duration: 1000
-      }
-    });
+  $('.submit-id').click(function(){
+  var messageId = $('.thread-id').val();
+    $.getJSON(
+         'https://graph.facebook.com/' + messageId + '/comments' +
+         '?format=json&access_token=' + token
+      )
+      .then(process)
+      .then(function() {
+        return messages;
+        // location.href = URL.createObjectURL(blob);
+      })
+      .then(function(messages) {
+        var chart = c3.generate({
+          bindto: '.container',
+          data: {
+            x: 'count',
+            columns: [
+              countWords(messages).axis,
+              countWords(messages).columns,
+            ],
+            groups: [
+              ['count']
+            ],
+            type: 'bar'
+          },
+          axis: {
+            x: {
+              type: 'categorized',
+            }
+          },
+          transition: {
+            duration: 1000
+          }
+        });
+      });
   });
+})
+;
