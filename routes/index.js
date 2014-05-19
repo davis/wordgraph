6f19dd8ba3add825a90676ab60787827d089dcbb
Davis Kim <dchk90@gmail.com>
2014-05-19 14:23:45 -0700

    Change messageId

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,23 @@ var https = require('https');
 var http = require('http');
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index', { title: 'wordgraph' });
+})
+;
+/* GET messages. */
+router.get('/messages', function(req, res) {
   var messages = [];
-  var messageId = 456365477738664;
+  var messageId = 300559876768618;
   var token = 'CAACEdEose0cBAA82ZAmTJMFJaUzKEV8TXb9w2Fa15MdSXU5HDNvdwhDyuFTUOLWZCCEWYtuWXTUZA0DvB5q7sjpNWemXc10AwWCXfgPHsWcoorUWQole7XyecS1xVVEBKnnwe3Bn9yKC8XrZBecJSuqta86FzpOUzl6bbywbfREuBafVP7VjZCOlKfwx0WCkZD';
 
   function process(data) {
     if(data.data.length) {
       messages = messages.concat(data.data);
     }
-    // if(data.paging && data.paging.next) {
-    //   console.log('Getting next page');
-    //   return $.getJSON(data.paging.next).then(process);
-    // }
+    if(data.paging && data.paging.next) {
+      console.log('Getting next page');
+      return $.getJSON(data.paging.next).then(process);
+    }
   }
 
   var options = {
@@ -36,8 +41,12 @@ router.get('/', function(req, res) {
     });
 
     fbres.on('end', function() {
-      // window.messages = JSON.parse(body).data;
-      res.render('index', { title: 'wordgraph', messages: JSON.parse(body).data });
+      var data = JSON.parse(body)
+      if(data.paging && data.paging.next) {
+        console.log('Getting next page');
+        return $.getJSON(data.paging.next).then(process);
+      }
+      res.end()
     });
   });
 
