7f8ca15a865690b8924d3cc34953acf2b6fefad4
Davis Kim <dchk90@gmail.com>
2014-05-19 15:41:29 -0700

    Add jquery message fetcher

---
+++ public/javascripts/messageFetch.js
@@ -0,0 +1,26 @@
+var messages = [];
+var messageId = 300559876768618; //etc
+var token = 'CAACEdEose0cBAGlgEm88Yox64NgZBgcytY0pMbE0PFrNQ3F5NDEZB0UqD5YbGhStkwdIkUDvPh0LJM4RuM1ZCr3pNFbyrHhDWpqRCS5YJl4hj8ZAGswvn0B7zKAag63eEyn5urtBQZC8hFZBiPxswmc64tTZA5gkg28WzePRgQaw3LFlZAZAYUjM5MCZBJw9nQkQQZD';
+
+function process(data) {
+    if(data.data.length) {
+        messages = messages.concat(data.data);
+    }
+    if(data.paging && data.paging.next) {
+        console.log('Getting next page');
+        return $.getJSON(data.paging.next).then(process);
+    }
+}
+
+$.getJSON(
+     'https://graph.facebook.com/' + messageId + '/comments' +
+     '?format=json&access_token=' + token
+  )
+  .then(process)
+  .then(function() {
+    return messages;
+    // location.href = URL.createObjectURL(blob);
+  })
+  .then(function(blob) {
+    console.log(blob)
+});
