8d3f70c061613e3b4a8797659de18954e187d9c1
Davis Kim <dchk90@gmail.com>
2014-05-19 12:22:54 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var https = require('https');
+var http = require('http');
+/* GET home page. */
+router.get('/', function(req, res) {
+  var messages = [];
+  var messageId = 456365477738664;
+  var token = 'CAACEdEose0cBAA82ZAmTJMFJaUzKEV8TXb9w2Fa15MdSXU5HDNvdwhDyuFTUOLWZCCEWYtuWXTUZA0DvB5q7sjpNWemXc10AwWCXfgPHsWcoorUWQole7XyecS1xVVEBKnnwe3Bn9yKC8XrZBecJSuqta86FzpOUzl6bbywbfREuBafVP7VjZCOlKfwx0WCkZD';
+
+  function process(data) {
+    if(data.data.length) {
+      messages = messages.concat(data.data);
+    }
+    // if(data.paging && data.paging.next) {
+    //   console.log('Getting next page');
+    //   return $.getJSON(data.paging.next).then(process);
+    // }
+  }
+
+  var options = {
+    hostname: 'graph.facebook.com',
+    // port: 443,
+    path: '/' + messageId + '/comments' + '?format=json&access_token=' + token,
+    method: 'GET'
+  };
+
+  var fbreq = https.request(options, function(fbres) {
+    console.log('statusCode: ', fbres.statusCode);
+    console.log('Headers: ', fbres.headers);
+
+    var body = '';
+
+    fbres.on('data', function(p) {
+      body += p;
+    });
+
+    fbres.on('end', function() {
+      // window.messages = JSON.parse(body).data;
+      res.render('index', { title: 'wordgraph', messages: JSON.parse(body).data });
+    });
+  });
+
+  fbreq.end();
+
+  fbreq.on('error', function(e) {
+    console.error(e);
+  });
+
+});
+
+module.exports = router;
