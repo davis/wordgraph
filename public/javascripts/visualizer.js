6bfcd17dd978ba241e010010b7bfee5d1864d95f
Davis Kim <dchk90@gmail.com>
2014-05-20 02:15:44 -0700

    Add visualizer

--- public/javascripts/visualizer.js
+++ public/javascripts/visualizer.js
@@ -4,55 +4,49 @@ var svg = d3.select('.container').append('svg')
 
 var common = ['the', 'that', 'in', 'it', 'is', 'we', 'all', 'a', 'an', 'by', 'to', 'you', 'me', 'he', 'she', 'they', 'we', 'how', 'it', 'i', 'are', 'to', 'for', 'of'];
 
-// trying to add occurence of word to an author's usecount -- proving to be very difficult
-function parseMessages(messages) {
-  console.log('messages', messages)
-  console.log(messages.to)
-  var people = {};
-  for(var person in messages.to.data) {
-    people[person.name] = { words: {} };
-  }
-  for(var message in messages.comments.data) {
-    var author = message.from.name;
-    var split = message.message.split(' ');
-    for(var word in split) {
-      if(people[author].words[word]) {
-        people[author].words[word]++;
-      } else {
-        people[author].words[word] = 1;
-      }
-    }
-  }
-  console.log(people)
-  return people;
-}
+function visualize(users, allWords) {
+  var xAxis = ['x'];
+  var userInfo = [];
+  var userList = [];
 
-// countWords: count words in an array of strings and return an object
-// with the counts of each word
-function countWords(messages) {
-  var words = {};
-  for(var i = 0; i < messages.length; i++) {
-    var split = messages[i].message.split(' ');
-    for(var j = 0; j < split.length; j++) {
-      if(!words[split[j]] && common.indexOf(split[j]) === -1) {
-        words[split[j]] = 1;
-      } else if(words[split[j]]){
-        words[split[j]]++;
+  for(var user in users) {
+    userInfo.push([user]);
+    userList.push(user);
+  }
+  console.log(userInfo);
+
+  for(var word in allWords) {
+    if(allWords[word] > 1) {
+      console.log('pushing ', word )
+      xAxis.push(word);
+      for (var i = 0; i < userInfo.length; i++) {
+        var count = users[userInfo[i][0]].words[word] || 0;
+        userInfo[i].push(count);
       }
     }
   }
 
-  var result = {
-    columns: ['total'],
-    axis: ['count']
-  };
-  for(var word in words) {
-    if(words[word] > 1) { // leaving out words that only happen once
-      result.columns.push(words[word]);
-      result.axis.push(word);
+  userInfo.unshift(xAxis);
+
+  console.log(userInfo);
+  console.log('visualizer.js - allWords', xAxis);
+
+  var chart = c3.generate({
+    bindto: '.container',
+    data: {
+        x : 'x',
+        columns: userInfo,
+        groups: [
+          userList
+        ],
+        type: 'bar'
+    },
+    axis: {
+        x: {
+            type: 'categorized' // this needed to load string x value
+        }
     }
-  }
-  return result;
+});
 }
 
 
