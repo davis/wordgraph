17b1e1770eddc22c60b1ce6e90e7a928a976b8ae
Davis Kim <dchk90@gmail.com>
2014-05-19 23:08:46 -0700

    Add fb login

---
+++ public/javascripts/login.js
@@ -0,0 +1,12 @@
+$('.login').click(function() {
+  FB.login(function(response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+      });
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  }, {scope: 'read_mailbox'});
+});
